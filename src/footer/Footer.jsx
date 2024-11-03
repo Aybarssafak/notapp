bb500f5ae72f3012c2b0ec757e48dad0d544139b
Aybarssafak <159555967+Aybarssafak@users.noreply.github.com>
2024-11-03 11:52:19 +0300

    İlk commit

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.js
+
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+        This is an Aybars Şafak Product.
+    </footer>
+  );
+}
+
+export default Footer;
