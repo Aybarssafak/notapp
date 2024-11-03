8026600dc347e5b8cdfe0c4d537db912ec56eb52
Aybarssafak <159555967+Aybarssafak@users.noreply.github.com>
2024-11-03 12:00:04 +0300

    Son değişiklik

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Notapp from './Notapp/Notapp.jsx';
 import CreateNot from './createNot/createNot.jsx';
 import Login from './login/login.jsx'
 import NoteDetail from './notedetali/NoteDetail.jsx';
+import Footer from './footer/Footer.jsx';
 
 function App() {
   return (
-      <Routes>
-        <Route path='/notapp' element={<Notapp />} />
-        <Route path='/create' element={<CreateNot />} />
-        <Route path='/' element={<Login />} />
-        <Route path="/note/:id" element={<NoteDetail />} />
-      </Routes>
+    <div>
+        <Routes>
+          <Route path='/notapp' element={<Notapp />} />
+          <Route path='/create' element={<CreateNot />} />
+          <Route path='/' element={<Login />} />
+          <Route path="/note/:id" element={<NoteDetail />} />
+        </Routes>
+        <Footer/>
+      </div>
   );
 }
 
